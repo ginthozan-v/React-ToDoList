3f62fe15f1ad653411fe9b4344b46fec03128f2f
Ginthozan <ginthozanv@gmail.com>
2021-08-19 13:41:41 +0530

    First commit

---
+++ frontend/src/components/ToDoCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { TrashIcon } from "@heroicons/react/solid";
+
+function ToDoCard({ id, title, complete, deleteTask }) {
+  return (
+    <div className="relative rounded-lg shadow-md bg-white px-5 py-2 cursor-pointer flex justify-between items-center focus:outline-none text-sm font-medium">
+      <div className="flex items-center">
+        <button className="w-5 h-5 mr-1" onClick={() => deleteTask(id)}>
+          <TrashIcon className=" text-red-500" />
+        </button>
+
+        {title}
+      </div>
+      <input
+        className="form-radio h-4 w-4"
+        type="radio"
+        name="radio"
+        id={title}
+        onClick={() => complete(id)}
+      />
+    </div>
+  );
+}
+
+export default ToDoCard;
